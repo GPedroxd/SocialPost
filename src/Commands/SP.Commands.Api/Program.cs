0004b53c8591772ab85b950d3682e0b6123e2b92
pedro santos <contato.tec.pedro@gmail.com>
2023-04-30 22:14:49 -0300

    change de docker compose file and fix appsettings configuration

--- src/Commands/SP.Commands.Api/Program.cs
+++ src/Commands/SP.Commands.Api/Program.cs
@@ -1,5 +1,9 @@
+using Confluent.Kafka;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using SP.Commands.Application.Handlers;
 using SP.Commands.Domain.Aggregates.PostAggregate;
+using SP.Commands.Infra.Config;
 using SP.Commands.Infra.Handlers;
 using SP.Commands.Infra.Producers;
 using SP.Commands.Infra.Repositories;
@@ -11,12 +15,17 @@ using SP.Core.Producers;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//difine how will be serialize guids atributtes, in this case will be as string
+BsonSerializer.RegisterSerializer(typeof(Guid), new GuidSerializer(MongoDB.Bson.BsonType.String));
+
 // Add services to the container.
 builder.Services.AddScoped<IEventStore, EventStore>();
 builder.Services.AddScoped<IEventProducer, EventProducer>();
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
- 
+builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection(nameof(ProducerConfig)));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
